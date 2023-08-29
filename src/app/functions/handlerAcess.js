7622ab908dda506714694cfc20b6574be4f2e79e
karen <karendecleva3@gmail.com>
2023-08-29 17:34:07 -0400

    localstore funcionando

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -3,16 +3,20 @@ import { validateToken } from "./validateToken";
 import { getUserAuthenticated } from "./handlerAcessAPI";
 
 const handlerAcessUser = async (user) => {
-
+    
     const userAuth = await getUserAuthenticated(user);
     
     const isTokenValidate = validateToken(userAuth.token);
 
     if (isTokenValidate) {
         Cookies.set('token', userAuth.token, { expires: 1 });
+        localStorage.setItem('name', userAuth.name);
     }
 
     return userAuth;
+
+
+
 }
 export default handlerAcessUser;
 
--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -2,9 +2,9 @@
 
 const user = [
     {
-        name: "karen",
-        email: "karen@gmail.com",
-        password: "karen123",
+        name: "joao",
+        email: "joao.santos@gmail.com",
+        password: "123",
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     },
 
