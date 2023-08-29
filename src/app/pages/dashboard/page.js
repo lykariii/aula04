675094fd9be0b4ecf0d1cd902ada264c20233a25
karen <karendecleva3@gmail.com>
2023-08-29 16:48:27 -0400

    list users

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,6 +1,7 @@
 
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
+import ListUsers from "@/app/componentes/ListUsers";
 
 export default async function Dashboard() {
    const users = await getUsers();
--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,18 @@ import { NextResponse } from "next/server";
 import { validateToken } from "./app/functions/validateToken";
 
 export const middleware = (request) => {
-
+    
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
-    const urlDashboard = new URL('/pages/dashboard', request.url);
     const isTokenValidated = validateToken(token);
+    const urlDashboard = new URL('/pages/dashboard', request.url)
 
     if (!isTokenValidated || !token) {
         if (request.nextUrl.pathname === '/pages/dashboard') {
             return NextResponse.redirect(urlLogin);
         }
     }
-
-        if (!isTokenValidated) {
+    if (isTokenValidated) {
         if (request.nextUrl.pathname === '/') {
             return NextResponse.redirect(urlDashboard);
         }
