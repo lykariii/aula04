cbdd306615ef0a93e39e03e4e8ce91d9c2332c4f
lykariii <karendecleva3@gmail.com>
2023-10-17 17:30:41 -0400

    Update handlerAcessAPI.js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,72 +1,23 @@
 'use server'
 
-const user = [
-    {
-        name: "joao",
-        email: "joao.santos@gmail.com",
-        password: "123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
+const url = "https://aula-17-10-peach.vercel.app"
 
+const getUserAuthenticated = async (user) => {
+    const responseOfApi = await fetch (url + "/user/authenticated",
     {
-        name: "karina",
-        email: "karina@gmail.com",
-        password: "karina123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "mendes",
-        email: "mendes@gmail.com",
-        password: "mendes123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "isabel",
-        email: "isabel@gmail.com",
-        password: "isabel123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "any",
-        email: "any@gmail.com",
-        password: "any123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "luiza",
-        email: "luiza@gmail.com",
-        password: "luiza123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "carlos",
-        email: "carlos@gmail.com",
-        password: "carlos123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-        name: "luna",
-        email: "luna@gmail.com",
-        password: "luna123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-]
-
-const getUserAuthenticated = (userLogin) => {
-    let usermach = [];
-
-    user.map((user) =>{
     
-    if(user.email === userLogin.email && user.password === userLogin.password){
-        usermach = user
-    }
-    })
-
-    return usermach
-   }
-
-const getUsers = () =>{
-    return user
+        method: "POST",
+        headers: { "Content-Typer": "Aplication/json" },
+        body: JSON.stringify(user)
         
+    }
+    );
+    const userAuth = await responseOfApi.json();
+    return userAuth
 }
+
+const getUsers = async () =>{
+    
+}
+
 export { getUsers, getUserAuthenticated };
